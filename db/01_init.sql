af8cb2a86519b9c965c3396e1d80f88bc23bdf9a
Chris <chrismachu@gmail.com>
2026-02-26 15:17:59 +0100

    incorporer postgres dans ci

--- db/01_init.sql
+++ db/01_init.sql
@@ -19,10 +19,10 @@ CREATE TABLE workshop_model_notions (
     CONSTRAINT fk_workshop_model_notions_workshop FOREIGN KEY (workshop_model_id) REFERENCES workshop_model(id)
 );
 
-INSERT INTO notion_model(name) VALUES ('Docker');
-INSERT INTO workshop_model(id, name) VALUES (2, 'Workshop 1');
-INSERT INTO workshop_model_notions(workshop_model_id, notions_name) VALUES (2, 'Docker');
+-- INSERT INTO notion_model(name) VALUES ('Docker');
+-- INSERT INTO workshop_model(id, name) VALUES (2, 'Workshop 1');
+-- INSERT INTO workshop_model_notions(workshop_model_id, notions_name) VALUES (2, 'Docker');
 
--- Aligner la séquence (id serial) après l'insert explicite id=2
-SELECT setval(pg_get_serial_sequence('workshop_model','id'), 2, true);
+-- -- Aligner la séquence (id serial) après l'insert explicite id=2
+-- SELECT setval(pg_get_serial_sequence('workshop_model','id'), 2, true);
 
